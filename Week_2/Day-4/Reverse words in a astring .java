2a3484f605dc253da1b2af71240abfd46ba50964
Nitya sharma <snitya583@gmail.com>
2026-06-12 20:31:32 +0530

    Update Reverse words in a astring .java

--- Week_2/Day-4/Reverse words in a astring .java
+++ Week_2/Day-4/Reverse words in a astring .java
@@ -1,1 +1,19 @@
+class Solution {
 
+    public String reverseWords(String s) {
+
+        String[] words = s.split(" ");
+
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+
+            result.append(
+                    new StringBuilder(word).reverse());
+
+            result.append(" ");
+        }
+
+        return result.toString().trim();
+    }
+}
