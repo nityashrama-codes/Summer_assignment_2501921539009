0bac2de391df6978423912f68883d158a14c5214
Nitya sharma <snitya583@gmail.com>
2026-06-12 20:32:00 +0530

    Update Decode string.java

--- Week_2/Day-4/Decode string.java
+++ Week_2/Day-4/Decode string.java
@@ -1,1 +1,45 @@
+class Solution {
 
+    public String decodeString(String s) {
+
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+
+        StringBuilder current = new StringBuilder();
+        int k = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isDigit(ch)) {
+
+                k = k * 10 + (ch - '0');
+
+            } else if (ch == '[') {
+
+                countStack.push(k);
+                stringStack.push(current);
+
+                current = new StringBuilder();
+                k = 0;
+
+            } else if (ch == ']') {
+
+                int count = countStack.pop();
+
+                StringBuilder temp =
+                        stringStack.pop();
+
+                while (count-- > 0)
+                    temp.append(current);
+
+                current = temp;
+
+            } else {
+
+                current.append(ch);
+            }
+        }
+
+        return current.toString();
+    }
+}
