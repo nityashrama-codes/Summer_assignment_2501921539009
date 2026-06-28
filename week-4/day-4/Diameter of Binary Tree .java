8374f00692c1f768cdd2cd8dfbcfefcdf325a449
Nitya sharma <snitya583@gmail.com>
2026-06-28 12:06:25 +0530

    Update Diameter of Binary Tree .java

--- week-4/day-4/Diameter of Binary Tree .java
+++ week-4/day-4/Diameter of Binary Tree .java
@@ -1,1 +1,24 @@
+class Solution {
 
+    int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+
+        depth(root);
+
+        return diameter;
+    }
+
+    private int depth(TreeNode node) {
+
+        if (node == null)
+            return 0;
+
+        int left = depth(node.left);
+        int right = depth(node.right);
+
+        diameter = Math.max(diameter, left + right);
+
+        return Math.max(left, right) + 1;
+    }
+}
