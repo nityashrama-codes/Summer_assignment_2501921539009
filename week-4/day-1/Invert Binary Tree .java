2ff308172375dcbb2618e4640e15d9343535e5d0
Nitya sharma <snitya583@gmail.com>
2026-06-28 12:00:47 +0530

    Update Invert Binary Tree .java

--- week-4/day-1/Invert Binary Tree .java
+++ week-4/day-1/Invert Binary Tree .java
@@ -1,1 +1,18 @@
+class Invert {
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root == null)
+            return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
 
